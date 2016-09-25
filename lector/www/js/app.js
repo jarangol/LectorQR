15537250d1093bcfaa4f2853de51617f236fdea3
jarangol <julian_arango08@hotmail.com>
2016-09-24 21:00:23 -0500

    Arreglando error al nombrar modulo del QR

--- lector/www/js/app.js
+++ lector/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('lectorQR', ['lector.controllers','ionic', 'starter.controllers', 'starter.services','ngCordova'])
+angular.module('lector', ['lector.controllers','ionic', 'starter.controllers', 'starter.services','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
